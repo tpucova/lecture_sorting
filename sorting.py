c5a6e3e608c8e407b5c8be1b168ae5f1a00c9499
tpucova <271400@vutbr.cz>
2025-04-14 13:58:54 +0200

    wth

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def read_data(file_name):
@@ -10,10 +11,35 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as csv_file:
+        data = csv.DictReader(csv_file)
+        for row in data:
+            print(row)
+    return data
+
+def selection_sorting(list_of_numbers):
+    for idx in range(len(list_of_numbers)):
+        min_idx = idx
+        for idx_new in range(idx + 1, len(list_of_numbers)):
+            if list_of_numbers[idx_new] < list_of_numbers[min_idx]:
+                min_idx = idx_new
+        list_of_numbers[idx], list_of_numbers[min_idx] = list_of_numbers[min_idx], list_of_numbers[idx]
+    return list_of_numbers
+
+def bubble_sort(sequence):
+    for idx in range(len(sequence) - 1):
+        for idx_new in range(len(sequence) - idx - 1):
+            if sequence[idx_new] > sequence[idx_new + 1]:
+                sequence[idx_new + 1], sequence[idx_new] = sequence[idx_new], sequence[idx_new + 1]
+    return sequence
 
 
 def main():
-    pass
+    my_data = read_data('numbers.csv')
+    print(my_data)
+    sorted_list = selection_sorting([2,5,1,3,8,45,12,4])
+    print(sorted_list)
+    print(bubble_sort([85,45,49,25,68]))
 
 
 if __name__ == '__main__':
