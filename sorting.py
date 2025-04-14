e0f6321e3a148564ff3fa68dc632c90a2130c964
tpucova <271400@vutbr.cz>
2025-04-14 14:29:00 +0200

    wtf'

--- sorting.py
+++ sorting.py
@@ -33,6 +33,15 @@ def bubble_sort(sequence):
                 sequence[idx_new + 1], sequence[idx_new] = sequence[idx_new], sequence[idx_new + 1]
     return sequence
 
+def insertion_sort(sequence):
+    for idx in range(1, len(sequence)):
+        value = sequence[idx]
+        j = idx - 1
+        while (sequence[j] > value) and (j >= 0):
+            sequence[j + 1] = sequence[j]
+            j = j - 1
+        sequence[j + 1] = value
+    return sequence
 
 def main():
     my_data = read_data('numbers.csv')
@@ -40,6 +49,7 @@ def main():
     sorted_list = selection_sorting([2,5,1,3,8,45,12,4])
     print(sorted_list)
     print(bubble_sort([85,45,49,25,68]))
+    print(insertion_sort([9, 5, 1, 4, 3]))
 
 
 if __name__ == '__main__':
